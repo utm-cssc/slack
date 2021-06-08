81745dbf9efbabedd2c7acafc03333a72d5a9203
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-06-08 08:28:14 -0400

    on modal submit skeleton

---
+++ constants.js
@@ -0,0 +1,59 @@
+const PILLARS = [
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Support and Guidance'
+		},
+		value: 'Support and Guidance'
+	},
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Academic and Professional Growth'
+		},
+		value: 'Academic and Professional Growth'
+	},
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Developing Technology Resources'
+		},
+		value: 'Developing Technology Resources'
+	},
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Collaboration with MCS Clubs and Societies'
+		},
+		value: 'Collaboration with MCS Clubs and Societies'
+	}
+];
+
+const TEAMS = [
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Communications'
+		},
+		value: 'Communications'
+	},
+	{
+		text: {
+			type: 'plain_text',
+			text: 'Operations'
+		},
+		value: 'Operations'
+	},
+    {
+		text: {
+			type: 'plain_text',
+			text: 'Technology'
+		},
+		value: 'Technology'
+	},
+];
+
+module.exports = {
+    PILLARS,
+    TEAMS
+};
--- index.js
+++ index.js
@@ -1,71 +1,136 @@
+const { PILLARS, TEAMS } = require('./constants');
 const { App } = require('@slack/bolt');
 require('dotenv').config();
 
 const app = new App({
 	token: process.env.TOKEN,
 	signingSecret: process.env.SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.APP_TOKEN
+	socketMode: true,
+	appToken: process.env.APP_TOKEN
 });
 
-app.command("/idea", async ({ ack, body, client }) => {
-    await ack();
-    try {
-        // Call views.open with the built-in client
-        const result = await client.views.open({
-          // Pass a valid trigger_id within 3 seconds of receiving it
-          trigger_id: body.trigger_id,
-          // View payload
-          view: {
-            type: 'modal',
-            // View identifier
-            callback_id: 'view_1',
-            title: {
-              type: 'plain_text',
-              text: 'Modal title'
-            },
-            blocks: [
-              {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text: 'Welcome to a modal with _blocks_'
-                },
-                accessory: {
-                  type: 'button',
-                  text: {
-                    type: 'plain_text',
-                    text: 'Click me!'
-                  },
-                  action_id: 'button_abc'
-                }
-              },
-              {
-                type: 'input',
-                block_id: 'input_c',
-                label: {
-                  type: 'plain_text',
-                  text: 'What are your hopes and dreams?'
-                },
-                element: {
-                  type: 'plain_text_input',
-                  action_id: 'dreamy_input',
-                  multiline: true
-                }
-              }
-            ],
-            submit: {
-              type: 'plain_text',
-              text: 'Submit'
-            }
-          }
-        });
-        console.log(result);
-      }
-      catch (error) {
-        console.error(error);
-      }
+app.command('/idea', async ({ ack, body, client }) => {
+	await ack();
+	try {
+		// Call views.open with the built-in client
+		const result = await client.views.open({
+			// Pass a valid trigger_id within 3 seconds of receiving it
+			trigger_id: body.trigger_id,
+			// View payload
+			view: {
+				type: 'modal',
+				// View identifier
+				callback_id: 'view_1',
+				title: {
+					type: 'plain_text',
+					text: 'Submit an idea'
+				},
+				blocks: [
+					{
+						type: 'input',
+						block_id: 'input_a',
+						label: {
+							type: 'plain_text',
+							text: 'What is your idea?'
+						},
+						element: {
+							type: 'plain_text_input',
+							action_id: 'dreamy_input',
+							multiline: true,
+						}
+					},
+					{
+						type: 'section',
+						block_id: 'input_b',
+						text: {
+							type: 'mrkdwn',
+							text: 'What pillar(s) does it fall under?'
+						},
+						accessory: {
+							action_id: 'text1234',
+							type: 'multi_static_select',
+							placeholder: {
+								type: 'plain_text',
+								text: 'Select a pillar'
+							},
+							options: PILLARS
+						}
+					},
+          {
+						type: 'input',
+						block_id: 'input_c',
+						label: {
+							type: 'plain_text',
+							text: 'What goal does it fall under?'
+						},
+						element: {
+							type: 'plain_text_input',
+							action_id: 'dreamy_input',
+							multiline: false
+						}
+					},
+          {
+						type: 'input',
+						block_id: 'input_d',
+						label: {
+							type: 'plain_text',
+							text: 'How does it compliment the pillar(s) and achieve the goal?'
+						},
+						element: {
+							type: 'plain_text_input',
+							action_id: 'dreamy_input',
+							multiline: true,
+						}
+					},
+          {
+						type: 'section',
+						block_id: 'input_e',
+						text: {
+							type: 'mrkdwn',
+							text: 'What team(s) would best execute this idea?'
+						},
+						accessory: {
+							action_id: 'text1234',
+							type: 'multi_static_select',
+							placeholder: {
+								type: 'plain_text',
+								text: 'Select a team'
+							},
+							options: TEAMS
+						}
+					},
+				],
+				submit: {
+					type: 'plain_text',
+					text: 'Submit'
+				}
+			}
+		});
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.view('view_1', async ({ ack, body, view, client}) => {
+  await ack();
+  const user = body['user']['id'];
+
+  let msg = '';
+  // Save to DB
+  //const results = await db.set(user.input, val);
+  msg = 'Yeehaw';
+
+  // Message the user
+  try {
+    await client.chat.postMessage({
+      channel: "ideas",
+      text: msg
     });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 
 (async () => {
 	const port = 3000;
